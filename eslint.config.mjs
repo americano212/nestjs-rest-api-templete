8e6ef3526800bdf94f9695cdcc8b037c8d715abc
americano212 <wq0212@naver.com>
2024-12-30 01:21:57 +0900

    style: Unify semi style

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,19 +1,19 @@
-import typescriptEslintEslintPlugin from '@typescript-eslint/eslint-plugin'
-import jest from 'eslint-plugin-jest'
-import globals from 'globals'
-import tsParser from '@typescript-eslint/parser'
-import path from 'node:path'
-import { fileURLToPath } from 'node:url'
-import js from '@eslint/js'
-import { FlatCompat } from '@eslint/eslintrc'
+import typescriptEslintEslintPlugin from '@typescript-eslint/eslint-plugin';
+import jest from 'eslint-plugin-jest';
+import globals from 'globals';
+import tsParser from '@typescript-eslint/parser';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import js from '@eslint/js';
+import { FlatCompat } from '@eslint/eslintrc';
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({
   baseDirectory: __dirname,
   recommendedConfig: js.configs.recommended,
   allConfig: js.configs.all,
-})
+});
 
 export default [
   {
@@ -47,7 +47,7 @@ export default [
     },
     files: ['src/**/*.ts', 'test/**/*.ts'],
     rules: {
-      '@typescript-eslint/explicit-function-return-type': 'warn',
+      '@typescript-eslint/explicit-function-return-type': 'off',
       '@typescript-eslint/explicit-module-boundary-types': 'off',
       '@typescript-eslint/no-explicit-any': 'off',
 
@@ -67,4 +67,4 @@ export default [
       ],
     },
   },
-]
+];
