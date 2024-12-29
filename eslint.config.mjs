a2510c6fe791c3627ee4a60b945b4eb8714f9b0c
americano212 <wq0212@naver.com>
2024-12-30 01:15:00 +0900

    style: Update eslint to v9

---
+++ eslint.config.mjs
@@ -0,0 +1,70 @@
+import typescriptEslintEslintPlugin from '@typescript-eslint/eslint-plugin'
+import jest from 'eslint-plugin-jest'
+import globals from 'globals'
+import tsParser from '@typescript-eslint/parser'
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+import js from '@eslint/js'
+import { FlatCompat } from '@eslint/eslintrc'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+})
+
+export default [
+  {
+    ignores: ['./eslint.eslintrc.mjs'],
+  },
+  ...compat.extends(
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+    'prettier',
+  ),
+  {
+    plugins: {
+      '@typescript-eslint': typescriptEslintEslintPlugin,
+      jest,
+    },
+
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.jest,
+      },
+
+      parser: tsParser,
+      ecmaVersion: 5,
+      sourceType: 'module',
+
+      parserOptions: {
+        project: './tsconfig.json',
+        tsconfigRootDir: './',
+      },
+    },
+    files: ['src/**/*.ts', 'test/**/*.ts'],
+    rules: {
+      '@typescript-eslint/explicit-function-return-type': 'warn',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+
+      'jest/expect-expect': [
+        'error',
+        {
+          assertFunctionNames: ['expect', 'request.*.expect'],
+        },
+      ],
+
+      'prettier/prettier': [
+        'error',
+        {
+          endOfLine: 'lf',
+          semi: true,
+        },
+      ],
+    },
+  },
+]
