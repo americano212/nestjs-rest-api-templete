2a1f98bf0b438eba3f7fe4be1f53c280d0e46dee
americano212 <wq0212@naver.com>
2024-12-31 22:49:49 +0900

    refactor: integrate enviroment value in config

--- src/config/envs/default.ts
+++ src/config/envs/default.ts
@@ -1,8 +1,39 @@
 export const config = {
   db: {
+    type: process.env['DB_TYPE'],
     entities: ['dist/**/*.entity.{ts,js}'],
   },
   bcrypt: {
     salt: Number(process.env['BCRYPT_SALT']) || 10,
+  },
+  api: {
+    port: process.env['PORT'],
+  },
+  jwt: {
+    accessTokenExpire: process.env['ACCESS_TOKEN_EXPIRE'] || '1d',
+    refreshTokenExpire: process.env['REFRESH_TOKEN_EXPIRE'] || '30d',
+  },
+  oauth: {
+    kakao: {
+      clientID: process.env['KAKAO_CLIENT_ID'],
+      clientSecret: process.env['KAKAO_CLIENT_SECRET'],
+      callbackURL: process.env['KAKAO_CALLBACK_URL'],
+    },
+    naver: {
+      clientID: process.env['NAVER_CLIENT_ID'],
+      clientSecret: process.env['NAVER_CLIENT_SECRET'],
+      callbackURL: process.env['NAVER_CALLBACK_URL'],
+    },
+  },
+  aws: {
+    accessKey: process.env['AWS_ACCESS_KEY_ID'] || '',
+    secretKey: process.env['AWS_SECRET_ACCESS_KEY'] || '',
+    region: process.env['AWS_REGION'],
+    s3: {
+      bucketName: process.env['AWS_S3_BUCKET_NAME'] || '',
+    },
+  },
+  slack: {
+    webhookUrl: process.env['SLACK_WEBHOOK_URL'] || '',
   },
 };
--- src/config/envs/production.ts
+++ src/config/envs/production.ts
@@ -1,6 +1,5 @@
 export const config = {
   db: {
-    type: process.env['DB_TYPE'] || 'mysql',
     synchronize: false,
     logging: false,
     host: process.env['DB_HOST'],
@@ -13,33 +12,5 @@ export const config = {
   jwt: {
     accessSecret: process.env['JWT_ACCESS_SECRET'],
     refreshSecret: process.env['JWT_REFRESH_SECRET'],
-    accessTokenExpire: process.env['ACCESS_TOKEN_EXPIRE'] || '1d',
-    refreshTokenExpire: process.env['REFRESH_TOKEN_EXPIRE'] || '30d',
-  },
-  api: {
-    port: process.env['PORT'],
-  },
-  aws: {
-    accessKey: process.env['AWS_ACCESS_KEY_ID'] || '',
-    secretKey: process.env['AWS_SECRET_ACCESS_KEY'] || '',
-    region: process.env['AWS_REGION'],
-    s3: {
-      bucketName: process.env['AWS_S3_BUCKET_NAME'] || '',
-    },
-    sms: {
-      account: process.env['MAIL_AWS_ACCOUNT'],
-      password: process.env['MAIL_AWS_PASSWORD'],
-      serever: process.env['MAIL_SERVER'],
-    },
-  },
-  slack: {
-    webhookUrl: process.env['SLACK_WEBHOOK_URL'] || '',
-  },
-  oauth: {
-    kakao: {
-      clientID: process.env['KAKAO_CLIENT_ID'],
-      clientSecret: process.env['KAKAO_CLIENT_SECRET'],
-      callbackURL: process.env['KAKAO_CALLBACK_URL'],
-    },
   },
 };
